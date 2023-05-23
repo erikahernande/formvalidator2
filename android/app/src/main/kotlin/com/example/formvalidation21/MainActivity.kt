dbdc8ed0965522ca035704e5a48cf54e4a9cb924
erikahernande <erika.hernandezmay@cbtis72.edu.mx>
2023-05-23 16:25:19 -0500

    app

---
+++ android/app/src/main/kotlin/com/example/formvalidation21/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.formvalidation221
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'package:formvalidation2/src/bloc/provider.dart';
+
+import 'package:formvalidation2/src/pages/home_page.dart';
+import 'package:formvalidation2/src/pages/login_page.dart';
+import 'package:formvalidation2/src/pages/producto_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'home',
+        routes: {
+          'login' : ( BuildContext context ) => LoginPage(),
+          'home'  : ( BuildContext context ) => HomePage(),
+          'producto' : ( BuildContext context ) => ProductoPage(),
+        },
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple,
+        ),
+      ),
+    );
+      
+  }
+}
